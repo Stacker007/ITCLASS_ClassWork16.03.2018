33e4d7bf42f7829cc57b2004eec09c9b2814e540
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-16 21:00:40 +0300

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char* s;
+	s = (char*)malloc(256*sizeof(char));
+	printf("Ââåäèòå ñòðîêó: ");
+	fgets(s,256,stdin);
+	if (s[strlen(s)-1]=='\n'){
+		s[strlen(s)-1]='\0';
+	}
+	s = (char*)realloc(s,(strlen(s)+1)*sizeof(char));
+	
+	for(int i = 0; i<strlen(s); i++){
+		if (s[i]>='a'&&s[i]<='z') s[i]-=('z'-'Z');
+		if (s[i]>='à'&&s[i]<='ÿ') s[i]-=('á'-'Á');
+	}
+	printf("Ïîëó÷àåòñÿ ñòðîêà: %s", s);
+	return(0);
+}
