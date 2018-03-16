f467a09b6130015d2f8d952d97ee3be996ce0634
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-16 20:23:42 +0300

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,1 +1,36 @@
-dsa
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n;
+	char c1,c2;
+	printf("Ââåäèòå N: ");
+	scanf("%d",&n );	
+	fflush(stdin);
+	printf("Ââåäèòå ñèìâîë 1: ");
+	scanf("%c",&c1);
+	fflush(stdin);
+	printf("Ââåäèòå ñèìâîë 2: ");
+	scanf("%c",&c2);
+		
+	char* s;	
+	s = (char*)malloc(n*sizeof(char));	
+	for (int i=0; i<n;i+=2){
+		s[i] = c1;
+		s[i+1] = c2;
+	}
+	printf("%s", s);
+
+	return(0);
+}
